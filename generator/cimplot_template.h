7c35e6061c676d78a93cdc5a958ae349dd60c5c6
Rirurin <rirurin@rirurin.com>
2025-06-19 20:21:36 +0800

    edits to get implot-rs bindgen working

--- generator/cimplot_template.h
+++ generator/cimplot_template.h
@@ -1,11 +1,23 @@
 #ifndef CIMGUIPLOT_INCLUDED
 #define CIMGUIPLOT_INCLUDED
 
-#include "cimgui.h"
+#include "../cimgui/cimgui.h"
 
 #ifdef CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include <time.h>
 typedef struct tm tm;
+
+typedef struct ImPlotPoint ImPlotPoint;
+struct ImPlotPoint
+{
+    double X, Y;
+}
+
+typedef struct ImPlotRange ImPlotRange;
+struct ImPlotRange
+{
+    double Min, Max;
+}
 
 #include "imgui_structs.h"
 #else
